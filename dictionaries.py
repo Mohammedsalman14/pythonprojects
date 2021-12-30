6ebd3fd724015d20db7221c04d98d9031f7d753d
mohammed salman <mohamedsalman140@gmail.com>
2021-12-30 19:57:00 +0530

    first projects

---
+++ dictionaries.py
@@ -0,0 +1,30 @@
+l=[
+ {"username":"salman",
+  "emailid":"mohammedsalman@gmail.com"},
+ 
+ 
+ 
+ {"username":"aadhil",
+ "emailid":"mohammedaadhil@gmail.com"},
+
+
+
+
+ {"username":"mohammedshariff",
+  "emailid":"mohammedshariff@gmail.com"},
+
+
+  {"username":"mohammed",
+    "emailid":"mohammed@gmail.com"
+
+  }
+  
+
+
+]
+for i in l:
+  print(i["username"])
+  print(end=" ")
+  print(i["emailid"])
+  
+  
